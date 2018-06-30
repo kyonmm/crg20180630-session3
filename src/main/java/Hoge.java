eed7186fbbf9467ba543060efd5b257503d4696a
Kyon MM <kyon.mm@gmail.com>
2018-06-30 13:02:39 +0900

    はじめてのJava

---
+++ src/main/java/Hoge.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class Hoge {
+  public List<Row> rows = new ArrayList<Row>();
+}
---
+++ src/main/java/Row.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class Row {
+  public List<Integer> cells = new ArrayList<Integer>();
+}
